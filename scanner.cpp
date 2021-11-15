448f06fe6bbf276d082a4229c0feadbe4a7e595d
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2021-11-15 23:59:20 +0200

    SIUUUUUUUUUUUUUUUUUUUUUU

---
+++ scanner.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+bool isReserved(string s) {
+    map<string, bool> reserved;
+    reserved["if"] = 1;
+    reserved["then"] = 1;
+    reserved["else"] = 1;
+    reserved["end"] = 1;
+    reserved["repeat"] = 1;
+    reserved["until"] = 1;
+    reserved["read"] = 1;
+    reserved["write"] = 1;
+    return reserved[s];
+}
+
+vector <vector<string>> string_parse(string tiny) {
+    string scannedChar = "";
+    vector <vector<string>> scanned(2);
+     
+    for (int i = 0; i < tiny.length(); i++) {
+        if (isalpha(tiny[i])) {
+            while(isalpha(tiny[i])){
+                scannedChar += tiny[i];
+                i++;
+            }
+            i--;
+            scanned[0].push_back(scannedChar);
+            if (isReserved(scannedChar)) {
+                scanned[1].push_back("reserved");
+            }
+            else {
+                scanned[1].push_back("identifier");
+            }
+            scannedChar = "";
+        }
+        else if (isdigit(tiny[i])) {
+            while (isdigit(tiny[i])) {
+                scannedChar += tiny[i];
+                i++;
+            }
+            i--;
+            scanned[0].push_back(scannedChar);
+            scanned[1].push_back("number");
+            scannedChar = "";
+        }
+        else if (isspace(tiny[i])) {
+            continue;
+        }
+        else if (tiny[i] == '{') {
+            while (tiny[i] != '}') {
+                i++;
+            }
+        }
+        else if (tiny[i] == ':' && tiny[i + 1] == '=') {
+            scanned[0].push_back(":=");
+            scanned[1].push_back("assign");
+            i++;
+        }
+        else if(tiny[i] == '+' || tiny[i] == '-' || tiny[i] == '/' || tiny[i] == '*' || tiny[i] == '=' || tiny[i] == '<' || tiny[i] == '(' || tiny[i] == ')' || tiny[i] == ';') {
+            scannedChar += tiny[i];
+            scanned[0].push_back(scannedChar);
+            scanned[1].push_back("Symbol");
+            scannedChar = "";
+        }
+    }
+    return scanned;
+}
+
+
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+            << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
+void fileOut(vector<vector<string>> s) {
+    ofstream myfile;
+    myfile.open("output.txt");
+    for (int i = 0; i < s[0].size(); i++) {
+        myfile << s[0][i] << ", " << s[1][i] << "\n";
+    }
+    myfile.close();
+}
+
+int main()
+{
+
+    string filename("input.txt");
+    string file_contents;
+
+    file_contents = readFileIntoString(filename);
+
+    vector<vector<string>> scanned;
+
+    scanned = string_parse(file_contents);
+
+    fileOut(scanned);
+
+    return 0;
+}
